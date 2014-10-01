8d18f10fd0dcd5bc26edc0d90c59ab61710593d8
pkura <przemyslaw.kura@docusafe.pl>
2014-10-01 19:54:12 +1000

    add new file for test

--- src/LabbdaTests.java
+++ src/LabbdaTests.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import java.time.LocalDate;
 import java.time.Month;
 
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,6 +18,10 @@ public class LabbdaTests {
 	public void test() {
 		LocalDate curseDate = LocalDate.of(2014, Month.JANUARY, 31);
 		
+		TrainigEvent trainingEvent = TrainigEvent.called("Bdd In action").scheduleFor(curseDate);
+		
+		LocalDate erlyBirdTicketsEnd = curseDate.minusMonths(1);
+		assertThat(trainingEvent.getErlybirdDateLimit(), is(equals(erlyBirdTicketsEnd)));
 		System.out.println(curseDate);
 	}
 
---
+++ src/TrainigEvent.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+
+public class TrainigEvent {
+
+	public static TrainigEvent called(String string) {
+		
+		return null;
+	}
+
+	public TrainigEvent scheduleFor(LocalDate curseDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getErlybirdDateLimit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
