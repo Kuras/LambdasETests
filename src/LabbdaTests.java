191527e99b11432f955240a8ed40b28a14270d3c
pkura <przemyslaw.kura@docusafe.pl>
2014-10-02 16:59:21 +1000

    some thing

--- src/LabbdaTests.java
+++ src/LabbdaTests.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.Month;
 
 import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,11 +19,13 @@ public class LabbdaTests {
 	public void test() {
 		LocalDate curseDate = LocalDate.of(2014, Month.JANUARY, 31);
 		
-		TrainigEvent trainingEvent = TrainigEvent.called("Bdd In action").scheduleFor(curseDate);
+//		TrainigEvent trainingEvent = TrainigEvent.called("Bdd In action").scheduleFor(curseDate);
 		
 		LocalDate erlyBirdTicketsEnd = curseDate.minusMonths(1);
-		assertThat(trainingEvent.getErlybirdDateLimit(), is(erlyBirdTicketsEnd));
-		assertThat(0, is(not(1)));
+String responseString = "color";
+		//		assertThat(trainingEvent.getErlybirdDateLimit(), is(erlyBirdTicketsEnd));
+		assertThat(responseString , anyOf(containsString("color"), containsString("colour")));
+		assertThat(1, is(equalTo(1)));
 		
 		System.out.println(curseDate);
 	}
